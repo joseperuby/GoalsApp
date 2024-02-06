7c10424e22419cec7867f03c61fdcecec835f516
José Luis Ramírez Larios <peperala34@gmail.com>
2024-02-06 09:54:24 -0600

    Pepe - HeaderLayout

--- GoalsApp/src/assets/components/shared/Header.jsx
+++ GoalsApp/src/assets/components/shared/Header.jsx
@@ -1,14 +1,18 @@
 import goals from '../img/objetivo.png'
+import profile from '../img/usuario.png'
+import './Header.css'
 
 function Header() {
     return (
-        <div>
-            <div>
-                <img src={goals} alt="objetivo" />
-                <a href="/">Goals App</a>
+        <div className="header">
+            <div className="logo">
+                <img className="logo-img" src={goals} alt="objetivo" />
+                <a className="app" href="/">Goals App</a>
             </div>
-            <nav className="flex">
-                <a href="/profila">Perfil</a>
+            <nav className="nav-profile">
+                <a  href="/profile">
+                    <img className="profile-img" src={profile} alt="profile" />
+                </a>
             </nav>
         </div>
     )
