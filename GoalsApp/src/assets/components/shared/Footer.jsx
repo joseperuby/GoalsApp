ee45ab3f7a97f47b8bdb2d962da537be5fb287ac
José Luis Ramírez Larios <peperala34@gmail.com>
2024-02-05 20:14:12 -0600

    Pepe - Tailwind installed

--- GoalsApp/src/assets/components/shared/Footer.jsx
+++ GoalsApp/src/assets/components/shared/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer>
+        <footer className="text-red-500">
             <small>Diseñado y Desarrollado por José Luis Ramirez Larios y Xavier Reyes Ochoa &copy;2024. Todos los derechos reservados</small>
         </footer>
     )
--- GoalsApp/src/assets/components/shared/Principal.jsx
+++ GoalsApp/src/assets/components/shared/Principal.jsx
@@ -1,4 +1,4 @@
-function Principal({children}) {
+function Principal({ children }) {
     return (
         <>
             <sidenav>
